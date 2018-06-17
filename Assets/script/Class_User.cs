a6755532decafcb24656fffd0dea22acc8fbe806
Kayupon1102 <34961853+Kayupon1102@users.noreply.github.com>
2018-06-17 17:33:13 +0900

    UserDataの保存(仮)
    JsonでUserDataを保存するようにしました。

---
+++ Assets/script/Class_User.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System;
+
+[Serializable]
+public class User
+{
+    public long ID;                      //ユーザーID
+    public string[] Questionnaires;             //アンケート
+    public DateTime[] Update = new DateTime[4]; //データベースの更新日
+
+
+}
